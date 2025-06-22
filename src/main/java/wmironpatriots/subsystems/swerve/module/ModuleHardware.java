862c789e8d4cea0ac52b1c8a17c866fb1939e8a7
Dasun L. Abeykoon <Dasun20202020@hotmail.com>
2025-06-22 01:32:52 +0000

    FEAT: Convert hardware interface to abstract class for monologue logging

--- src/main/java/wmironpatriots/subsystems/swerve/module/ModuleHardware.java
+++ src/main/java/wmironpatriots/subsystems/swerve/module/ModuleHardware.java
@@ -1,52 +1,63 @@
 package wmironpatriots.subsystems.swerve.module;
 
+import monologue.Annotations.Log;
+
 /** Generalized Hardware methods for a Swerve Module */
-public interface ModuleHardware {
+public abstract class ModuleHardware {
+    @Log public double pivotPoseRevs;
+    @Log public double driveSpeedMps;
+    @Log public double pivotSetpointPoseRevs;
+    @Log public double driveSetpointSpeedMps;
+
     /**
      * @return pivot motor's position in Revolutions
      */
-    public double getPivotPoseRevs();
+    public double getPivotPoseRevs() {
+        return pivotPoseRevs;
+    }
 
     /**
      * @return drive motor's speed in Meters Per Second
      */
-    public double getDriveSpeedMps();
+    public double getDriveSpeedMps() {
+        return driveSpeedMps;
+    }
 
     /**
      * Set pivot motor's voltage setpoint
      * 
      * @param volts desired voltage setpoint
      */
-    public void setPivotAppliedVolts(double volts);
+    public abstract void setPivotAppliedVolts(double volts);
 
     /**
      * Set drive motor's voltage setpoint
      * 
      * @param volts desired voltage setpoint
      */
-    public void setDriveAppliedVolts(double volts);
+    public abstract void setDriveAppliedVolts(double volts);
 
     /**
      * Set pivot motor's position setpoint
      * 
      * @param volts desired position setpoint in Revolutions
      */
-    public void setPivotSetpointPose(double poseRevs);
+    public abstract void setPivotSetpointPose(double poseRevs);
 
     /**
      * Set drive motor's speed setpoint
      * 
      * @param volts desired speed setpoint in MMeters Per Second
      */
-    public void setDriveSetpointSpeed(double speedMps);
+    public abstract void setDriveSetpointSpeed(double speedMps);
 
     /** Stop both pivot and drive motors */
-    public void stop();
+    public abstract void stop();
     
     /**
      * Toggle motor coasting
      * 
      * @param enabled should coasting be enabled?
      */
-    public void coastingEnabled(boolean enabled);
+    public abstract void coastingEnabled(boolean enabled);
 }
