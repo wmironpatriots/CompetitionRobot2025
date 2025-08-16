b063df40e4ee5d22d79b72540b56f504899537f4
Dasun Abeykoon <Dasun20202020@hotmail.com>
2025-08-16 13:10:53 -0400

    fix: update class paths
    Signed-off-by: Dasun Abeykoon <Dasun20202020@hotmail.com>

--- src/main/java/org/frc6423/robot/Robot.java
+++ src/main/java/org/frc6423/robot/Robot.java
@@ -13,6 +13,7 @@ import edu.wpi.first.wpilibj.Alert.AlertType;
 import edu.wpi.first.wpilibj.DriverStation;
 import edu.wpi.first.wpilibj.RobotController;
 import edu.wpi.first.wpilibj.XboxController;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.Commands;
 import org.frc6423.lib.CommandRobot;
