fd46cbb205da239c1347aba69392ab4cf257bbb6
Dasun Abeykoon <Dasun20202020@hotmail.com>
2025-08-24 15:51:22 -0400

    feat: create Arm subsystem hardware interface
    Signed-off-by: Dasun Abeykoon <Dasun20202020@hotmail.com>

---
+++ src/main/java/org/frc6423/robot/subsystems/arm/ArmIO.java
@@ -0,0 +1,74 @@
+// Copyright (c) 2025 FRC 6423 - Ward Melville Iron Patriots
+// https://github.com/wmironpatriots
+// 
+// Open Source Software; you can modify and/or share it under the terms of
+// MIT license file in the root directory of this project
+
+package org.frc6423.robot.subsystems.arm;
+
+import edu.wpi.first.epilogue.Logged;
+
+/** Generalized hardware methods for {@link Arm} subsystem */
+public interface ArmIO extends AutoCloseable {
+  /** Run periodic hardware logic */
+  public void periodic();
+
+  /**
+   * @return angle of pivot motor in radians
+   */
+  @Logged(importance = Logged.Importance.INFO)
+  public double getPivotAngleRads();
+
+  /**
+   * @return setpoint angle of pivot motor in radians
+   */
+  @Logged(importance = Logged.Importance.INFO)
+  public double getPivotSetpointAngleRads();
+
+  /**
+   * @return stator current of pivot motor
+   */
+  @Logged(importance = Logged.Importance.INFO)
+  public double getPivotStatorCurrentAmps();
+
+  /**
+   * @return stator current of roller motor
+   */
+  @Logged(importance = Logged.Importance.INFO)
+  public double getRollerStatorCurrentAmps();
+
+  /**
+   * Reset pivot motor relative encoder to specified position
+   *
+   * @param poseRads position to reset to
+   */
+  public void resetPivotEncoder(double poseRads);
+
+  /**
+   * Run pivot motor at specified voltage setpoint
+   *
+   * @param volts desired voltage
+   */
+  public void runPivotVolts(double volts);
+
+  /**
+   * Run pivot motor to specified angle using onboard closed-loop control
+   *
+   * @param angleRads desired position in radians
+   */
+  public void runPivotAngle(double angleRads);
+
+  /**
+   * Run roller motor at specified voltage setpoint
+   *
+   * @param volts desired voltage
+   */
+  public void runRollerVolts(double volts);
+
+  /**
+   * Run roller motor at specific speed using onboard open-loop control
+   *
+   * @param speedRpm desired speed in revs per minute
+   */
+  public void runRollerSpeed(double speedRpm);
+}
