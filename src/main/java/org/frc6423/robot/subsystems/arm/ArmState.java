30e4293a7ec730acf0344278cc54b9417f9b946d
Dasun Abeykoon <Dasun20202020@hotmail.com>
2025-08-26 12:14:45 -0400

    doc: ArmState javadoc edit

--- src/main/java/org/frc6423/robot/subsystems/arm/ArmState.java
+++ src/main/java/org/frc6423/robot/subsystems/arm/ArmState.java
@@ -9,26 +9,26 @@ package org.frc6423.robot.subsystems.arm;
 import edu.wpi.first.math.geometry.Rotation2d;
 
 /** Represents a state the {@link Arm} subsystem can be in */
-// TODO put actual angles
+// TODO CHECK ROLLER SPEEDS
 public enum ArmState {
   /** Resting state */
-  STOWED(Rotation2d.fromDegrees(0), 0.0),
+  STOWED(Rotation2d.fromDegrees(-90), 0.0),
   /** Avoidance state for preventing collisions */
-  AVOIDING(Rotation2d.fromDegrees(20), 0.0),
+  AVOIDING(Rotation2d.fromDegrees(-72), 0.0),
   /** Flipped state for intaking */
-  INTAKING(Rotation2d.fromDegrees(0.0), 0.0),
+  INTAKING(Rotation2d.fromDegrees(90), 20.0),
   /** L2 pose but not scoring */
-  L2_PRIMED(Rotation2d.fromDegrees(0.0), 0.0),
+  L2_PRIMED(Rotation2d.fromDegrees(21.975), 0.0),
   /** L3 pose but not scoring */
-  L3_PRIMED(Rotation2d.fromDegrees(0.0), 0.0),
+  L3_PRIMED(Rotation2d.fromDegrees(21.975), 0.0),
   /** L4 pose but not scoring */
-  L4_PRIMED(Rotation2d.fromDegrees(0.0), 0.0),
+  L4_PRIMED(Rotation2d.fromDegrees(0.0), 0.0), // TODO L4 ANGLE
   /** L2 scoring */
-  L2_SCORING(Rotation2d.fromDegrees(0.0), 0.0),
+  L2_SCORING(Rotation2d.fromDegrees(21.975), -40.0),
   /** L3 scoring */
-  L3_SCORING(Rotation2d.fromDegrees(0.0), 0.0),
+  L3_SCORING(Rotation2d.fromDegrees(21.975), -40.0),
   /** L4 scoring */
-  L4_SCORING(Rotation2d.fromDegrees(0.0), 0.0);
+  L4_SCORING(Rotation2d.fromDegrees(0.0), -40.0); // TODO L4 ANGLE
 
   public final Rotation2d angle;
   public final double rollerSpeedRpm;
