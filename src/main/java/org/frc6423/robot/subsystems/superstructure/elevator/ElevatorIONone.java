96e13f851c4c16ad55a8a3418cbee1f883de3a37
Dasun Abeykoon <Dasun20202020@hotmail.com>
2025-08-26 18:39:59 -0400

    feat: create empty ElevatorIO
    Signed-off-by: Dasun Abeykoon <Dasun20202020@hotmail.com>

---
+++ src/main/java/org/frc6423/robot/subsystems/superstructure/elevator/ElevatorIONone.java
@@ -0,0 +1,62 @@
+// Copyright (c) 2025 FRC 6423 - Ward Melville Iron Patriots
+// https://github.com/wmironpatriots
+// 
+// Open Source Software; you can modify and/or share it under the terms of
+// MIT license file in the root directory of this project
+
+package org.frc6423.robot.subsystems.superstructure.elevator;
+
+/** Null {@link ElevatorIO} */
+public class ElevatorIONone implements ElevatorIO {
+  public ElevatorIONone() {}
+
+  @Override
+  public void periodic() {}
+
+  @Override
+  public double getParentPoseMeters() {
+    return 0.0;
+  }
+
+  @Override
+  public double getChildPoseMeters() {
+    return 0.0;
+  }
+
+  @Override
+  public double getSetpointPoseMeters() {
+    return 0.0;
+  }
+
+  @Override
+  public double getParentStatorCurrentAmps() {
+    return 0.0;
+  }
+
+  @Override
+  public double getChildStatorCurrentAmps() {
+    return 0.0;
+  }
+
+  @Override
+  public double getParentTempCelsius() {
+    return 0.0;
+  }
+
+  @Override
+  public double getChildTempCelsius() {
+    return 0.0;
+  }
+
+  @Override
+  public void resetEncoders(double poseMeters) {}
+
+  @Override
+  public void setVolts(double volts) {}
+
+  @Override
+  public void setPose(double poseMeters, double accelerationMpsSqrd) {}
+
+  @Override
+  public void close() throws Exception {}
+}
