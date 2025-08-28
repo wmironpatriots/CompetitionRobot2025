58a92b65af3e7038ceb7b9ec77cb271cf88f15c5
Dasun Abeykoon <Dasun20202020@hotmail.com>
2025-08-28 00:02:27 -0400

    refactor: rename ElevatorState enum to ElevatorExtension
    Signed-off-by: Dasun Abeykoon <Dasun20202020@hotmail.com>

--- src/main/java/org/frc6423/robot/subsystems/superstructure/elevator/ElevatorExtension.java
+++ src/main/java/org/frc6423/robot/subsystems/superstructure/elevator/ElevatorExtension.java
@@ -6,24 +6,25 @@
 
 package org.frc6423.robot.subsystems.superstructure.elevator;
 
+import static edu.wpi.first.units.Units.Inches;
 import static edu.wpi.first.units.Units.Meters;
 
 import edu.wpi.first.units.measure.Distance;
 
-/** Represents a state the {@link Elevator} subsystem can be in */
-public enum ElevatorState {
+/** Represents an extension height the {@link Elevator} subsystem can be in */
+public enum ElevatorExtension {
   /** Resting state */
   STOWED(Meters.of(0.0)),
   /** L2 height */
-  L2(Meters.of(0.0)),
+  L2(Inches.of(4.549)),
   /** L3 height */
-  L3(Meters.of(0.0)),
+  L3(Inches.of(12.41)),
   /** L4 height */
-  L4(Meters.of(0.0));
+  L4(Inches.of(24.0));
 
-  public final Distance extension;
+  public final Distance height;
 
-  private ElevatorState(Distance extension) {
-    this.extension = extension;
+  private ElevatorExtension(Distance height) {
+    this.height = height;
   }
 }
