e54ae948f32005aa93bf089e3ace54971e5919da
Dasun Abeykoon <Dasun20202020@hotmail.com>
2025-08-29 23:37:29 -0400

    feat: create arm subsystem factory
    Signed-off-by: Dasun Abeykoon <Dasun20202020@hotmail.com>

--- src/main/java/org/frc6423/robot/subsystems/superstructure/arm/Arm.java
+++ src/main/java/org/frc6423/robot/subsystems/superstructure/arm/Arm.java
@@ -6,11 +6,20 @@
 
 package org.frc6423.robot.subsystems.superstructure.arm;
 
+import static edu.wpi.first.units.Units.RPM;
+import static edu.wpi.first.units.Units.Radians;
+
 import edu.wpi.first.epilogue.Logged;
+import edu.wpi.first.units.measure.Angle;
+import edu.wpi.first.units.measure.AngularVelocity;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.Commands;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import java.util.function.DoubleSupplier;
 import org.frc6423.lib.subsystems.RollerIO;
+import org.frc6423.lib.subsystems.RollerIONeo;
+import org.frc6423.lib.subsystems.RollerIONone;
+import org.frc6423.robot.Robot;
 
 /** Arm Subsystem */
 public class Arm extends SubsystemBase {
@@ -23,7 +32,27 @@ public class Arm extends SubsystemBase {
   @Logged(name = "Arm Setpoint State")
   private ArmState setpointState = ArmState.STOWED;
 
-  public Arm(ArmPivotIO pivotHardware, RollerIO rollerHardware) {
+  /**
+   * @return fake {@link Arm} subsystem
+   */
+  public Arm none() {
+    return new Arm(new ArmPivotIONone(), new RollerIONone());
+  }
+
+  /**
+   * Factory for creating a {@link Arm} subsystem
+   *
+   * @return {@link Arm} Subsystem
+   */
+  public Arm create() {
+    if (Robot.isReal()) {
+      return new Arm(new ArmPivotIOReal(), new RollerIONeo());
+    } else {
+      return new Arm(new ArmPivotIOSim(), new RollerIONone());
+    }
+  }
+
+  private Arm(ArmPivotIO pivotHardware, RollerIO rollerHardware) {
     this.pivot = new ArmPivot(pivotHardware);
     this.roller = new ArmRoller(rollerHardware);
   }
@@ -46,25 +75,48 @@ public class Arm extends SubsystemBase {
   }
 
   /**
+   * Run arm to specified state
+   *
+   * @param pivotAngle {@link Angle} representing desired pivot angle
+   * @param rollerSpeed {@link AngularVelocity} representing desired roller speed
+   * @return {@link Command}
+   */
+  public Command runState(Angle pivotAngle, AngularVelocity rollerSpeed) {
+    return Commands.parallel(
+        pivot.runAngle(pivotAngle.in(Radians)), roller.runSpeed(rollerSpeed.in(RPM)));
+  }
+
+  /**
+   * Run arm to specified state
+   *
+   * @param pivotAngleRads desired pivot angle in radians
+   * @param rollerSpeed desired roller speed in radians
+   * @return {@link Command}
+   */
+  public Command runState(DoubleSupplier pivotAngleRads, DoubleSupplier rollerSpeedRpm) {
+    return Commands.parallel(
+        pivot.runAngle(pivotAngleRads.getAsDouble()),
+        roller.runSpeed(rollerSpeedRpm.getAsDouble()));
+  }
+
+  /**
+   * Run arm to specified state
+   *
+   * @param pivotAngleRads desired pivot angle in radians
+   * @param rollerSpeed desired roller speed in radians
+   * @return {@link Command}
+   */
+  public Command runState(double pivotAngleRads, double rollerSpeedRpm) {
+    return this.runState(() -> pivotAngleRads, () -> rollerSpeedRpm);
+  }
+
+  /**
    * Run arm to specified {@link ArmState}
    *
    * @param state desired {@link ArmState}
    * @return {@link Command}
    */
   public Command runState(ArmState state) {
-    return Commands.parallel(
-        this.run(() -> setpointState = state).until(() -> true),
-        pivot.runAngle(state.angle.getMeasure()),
-        roller.runSpeed(state.rollerSpeedRpm));
-  }
-
-  /**
-   * Hold arm at current angle and roller speed
-   *
-   * @return {@link Command}
-   */
-  public Command holdState() {
-    return Commands.parallel(
-        this.run(() -> {}).until(() -> true), pivot.holdAngle(), roller.holdSpeed());
+    return this.runState(state.angle.getRadians(), state.rollerSpeedRpm);
   }
 }
